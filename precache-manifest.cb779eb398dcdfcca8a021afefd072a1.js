a7911d6427ea803da5e4b3421b77776a89c647ef
Jaswanth N <jaswanth.27@gmail.com>
2022-02-26 12:37:46 +0530

    Updates

---
+++ precache-manifest.cb779eb398dcdfcca8a021afefd072a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "144d05feb6792f57b6e1",
+    "url": "/imjessy08/nameit/static/css/main.5441fb11.chunk.css"
+  },
+  {
+    "revision": "144d05feb6792f57b6e1",
+    "url": "/imjessy08/nameit/static/js/main.144d05fe.chunk.js"
+  },
+  {
+    "revision": "f2713309216dac7541b2",
+    "url": "/imjessy08/nameit/static/js/1.f2713309.chunk.js"
+  },
+  {
+    "revision": "804582a5ab417fd6f267",
+    "url": "/imjessy08/nameit/static/js/2.804582a5.chunk.js"
+  },
+  {
+    "revision": "83c6c40f74356f394953",
+    "url": "/imjessy08/nameit/static/js/runtime~main.83c6c40f.js"
+  },
+  {
+    "revision": "9f0260e923d77d94e12cb3da3dc43ce5",
+    "url": "/imjessy08/nameit/index.html"
+  }
+];
